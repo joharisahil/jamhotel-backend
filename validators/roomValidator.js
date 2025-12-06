cb73ca642bc7005e67f00c522639a6e5a666b9e5
Sahil Johari <sahiljohari04@gmail.com>
2025-12-06 12:11:40 +0530

    added validation in in room

--- validators/roomValidator.js
+++ validators/roomValidator.js
@@ -6,5 +6,10 @@ export const createRoomSchema = z.object({
   floor: z.number().optional(),
   baseRate: z.number().optional(),
   maxGuests: z.number().min(1).optional(),
-  plans: z.array(z.object({ code: z.string(), name: z.string(), rate: z.number() })).optional()
+  plans: z.array(z.object({
+    code: z.string(),
+    name: z.string(),
+    singlePrice: z.number(), 
+    doublePrice: z.number()  
+  })).optional()
 });
